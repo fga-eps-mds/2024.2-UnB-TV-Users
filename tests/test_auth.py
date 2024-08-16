0a27c4756e9cc9358219c0cd5a64ef1e57721742
gabrielhrlima <ghrl2003@gmail.com>
2024-08-16 15:46:25 -0300

    Testa o endpoint que cria o super-admin

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -234,3 +234,23 @@ class TestAuth:
         data = response.json()
         assert response.status_code == 400
         assert data['detail'] == "Account is not @unb"
+
+    # SUPER ADMIN SETUP
+    def test_admin_setup(self, setup):
+        # Testa a tentativa com e-mail inválido
+        response = client.post("/api/auth/super-admin-setup", json={"email": invalid_connection['email']})
+        data = response.json()
+        assert response.status_code == 404
+        assert data['detail'] == errorMessages.USER_NOT_FOUND
+
+        # Testa a tentativa com usuário inativo
+        response = client.post("/api/auth/super-admin-setup", json={"email": valid_user_not_active['email']})
+        data = response.json()
+        assert response.status_code == 400
+        assert data['detail'] == "Account is not active"
+
+        # Testa a tentativa com e-mail que não contém "unb"
+        response = client.post("/api/auth/super-admin-setup", json={"email": valid_user_active_user['email']})
+        data = response.json()
+        assert response.status_code == 400
+        assert data['detail'] == "Account is not @unb"
